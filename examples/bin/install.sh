4f92b973cc153e91e3b555e29b884839d71f6395
joshcough <joshcough@gmail.com>
2012-05-06 11:02:46 -0400

    added some boolean parsers added ability to not

--- examples/bin/install.sh
+++ examples/bin/install.sh
@@ -7,19 +7,19 @@ curl -L "http://dl.bukkit.org/latest-rb/craftbukkit.jar" -o craftbukkit.jar
 cp craftbukkit.jar lib/
 cd lib
 
-# get scala
-curl "http://repo1.maven.org/maven2/org/scala-lang/scala-library/2.9.1-1/scala-library-2.9.1-1.jar" -o scala-library-2.9.1-1.jar
+# get scala (not getting it anymore because its in netlogo. leaving this here for reference though)
+#curl "http://repo1.maven.org/maven2/org/scala-lang/scala-library/2.9.1-1/scala-library-2.9.1-1.jar" -o scala-library-2.9.1-1.jar
 
 # get npc creatures (needed by the netlogo plugin)
 curl "http://dev.bukkit.org/media/files/584/232/NPCCreatures.jar" -o NPCCreatures.jar
 
-# get NetLogo
+# get NetLogo (which nicely happens to include scala, so we don't have to download that anymore)
 curl "http://ccl.northwestern.edu/netlogo/5.0.1/netlogo-5.0.1.tar.gz" -o netlogo-5.0.1.tar.gz
 
 # get jcdc plugin factor library
-#"jcdc-plugin-factory_2.9.1-1" % "0.1"
+curl "https://github.com/downloads/joshcough/MinecraftPlugins/jcdc-plugin-factory-0.1.jar" -o jcdc-plugin-factory-0.1.jar
 
 # unpack NetLogo
 tar xfvz netlogo-5.0.1.tar.gz
 cp netlogo-5.0.1/NetLogo.jar .
-cp -r netlogo-5.0.1/lib .
+cp -r netlogo-5.0.1/lib/* .
