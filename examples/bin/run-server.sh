7a3c567df8848a14fbf0e6e695b3c0427e1cef01
joshcough <joshcough@gmail.com>
2012-05-06 10:10:06 -0400

    cleaned up some jar names

--- examples/bin/run-server.sh
+++ examples/bin/run-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $BUKKIT
-java -Xmx1024M -Xms512M -cp "craftbukkit-1.2.5-R1.0.jar:scala-library-2.9.1-1.jar:jcdc-plugin-factory_2.9.1-1-0.1.jar:npccreatures-dev-SNAPSHOT.jar:NetLogo.jar" org.bukkit.craftbukkit.Main
+java -Xmx1024M -Xms512M -cp "craftbukkit.jar:scala-library-2.9.1-1.jar:jcdc-plugin-factory-0.1.jar:NPCCreatures.jar:NetLogo.jar" org.bukkit.craftbukkit.Main
