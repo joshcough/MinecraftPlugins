b2d622200f3974cb86b11db34e63e95fc29f0ac8
joshcough <joshcough@gmail.com>
2012-05-06 09:48:01 -0400

    added install script

---
+++ examples/bin/install.sh
@@ -0,0 +1,23 @@
+# put everything in a bukkit dir so we don't pollute the users current directory.
+mkdir bukkit
+cd bukkit
+
+# get bukkit (the minecraft server)
+curl -L "http://dl.bukkit.org/latest-rb/craftbukkit.jar" -o craftbukkit.jar
+
+# get scala
+curl "http://repo1.maven.org/maven2/org/scala-lang/scala-compiler/2.9.1-1/scala-compiler-2.9.1-1.jar" -o scala-compiler-2.9.1-1.jar
+
+# get npc creatures (needed by the netlogo plugin)
+curl "http://dev.bukkit.org/media/files/584/232/NPCCreatures.jar" -o NPCCreatures.jar
+
+# get NetLogo
+curl "http://ccl.northwestern.edu/netlogo/5.0.1/netlogo-5.0.1.tar.gz" -o netlogo-5.0.1.tar.gz
+
+# get jcdc plugin factor library
+#"jcdc-plugin-factory_2.9.1-1" % "0.1"
+
+# unpack NetLogo
+tar xfvz netlogo-5.0.1.tar.gz
+cp netlogo-5.0.1/NetLogo.jar .
+cp -r netlogo-5.0.1/lib .
