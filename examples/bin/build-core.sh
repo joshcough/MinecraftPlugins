85338ba8c6533d533cf60a17a728e1985f9f17a4
joshcough <joshcough@gmail.com>
2012-05-06 10:20:16 -0400

    minor fix

--- examples/bin/build-core.sh
+++ examples/bin/build-core.sh
@@ -1,6 +1,5 @@
 # first build core
-export BUKKIT=`pwd`/lib
 cd ../core
 ./sbt package
 cd ../examples
-cp ../core/target/scala-2.9.1-1/jcdc-plugin-factory_2.9.1-1-0.1.jar lib
+cp ../core/target/scala-2.9.1-1/jcdc-plugin-factory_2.9.1-1-0.1.jar $BUKKIT/jcdc-plugin-factory-0.1.jar
