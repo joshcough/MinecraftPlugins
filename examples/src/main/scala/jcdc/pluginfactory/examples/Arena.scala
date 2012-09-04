12c9091bbe1a27e73dcbe7a8b2d3ff61df7eab50
Josh Cough <joshcough@gmail.com>
2012-09-04 12:37:54 -0400

    changed some foreachs to for comprehensions

--- examples/src/main/scala/jcdc/pluginfactory/examples/Arena.scala
+++ examples/src/main/scala/jcdc/pluginfactory/examples/Arena.scala
@@ -13,26 +13,28 @@ class Arena extends ListenersPlugin with CommandsPlugin with Cubes {
     }),
     OnRightClickBlock((p, e) =>
       if (p isHoldingA STONE_AXE) setSecondPosition(p, e.loc)),
-    OnPlayerMove((p, e) => cubes.foreach{ case (cp, cube) =>
+    OnPlayerMove((p, e) => for((cp, cube) <- cubes)
       if (cube.contains(e.getTo) and ! cube.contains(e.getFrom))
         p ! ("You are entering the battle arena of " + cp.name + "!")
       else if (cube.contains(e.getFrom) and ! cube.contains(e.getTo))
         p ! ("You are leaving the battle arena of " + cp.name + "!")
-    })
+    )
   )
 
   val commands = List(
     Command(
-      name = "/axe",
+      name = "axe",
       desc = "Get an Arena wand.",
       body = noArgs(_.loc.dropItem(STONE_AXE))
     ),
     Command(
-      name = "/arena",
+      name = "arena",
       desc = "Set all the selected blocks to the given material type.",
-      body = args(material) { case p ~ m => run(p)(cube => cube.blocks.foreach(b =>
-        if (cube.onWall(b) or cube.onFloor(b)) b changeTo m else b changeTo AIR
-      ))}
+      body = args(material) { case p ~ m => run(p){ cube =>
+        for(b <- cube.blocks)
+          if(cube.onWall(b) or cube.onFloor(b)) b changeTo m else b changeTo AIR
+        () // shut up intellij
+      }}
     )
   )
 }
--- examples/src/main/scala/jcdc/pluginfactory/examples/GetOffMyLawn.scala
+++ examples/src/main/scala/jcdc/pluginfactory/examples/GetOffMyLawn.scala
@@ -1,22 +1,31 @@
 package jcdc.pluginfactory.examples
 
-import jcdc.pluginfactory.{Cubes, Command, CommandsPlugin, CommonCommands, ListenerPlugin}
-import CommonCommands._
+import jcdc.pluginfactory._
+import org.bukkit.event.player.PlayerMoveEvent
 
 class GetOffMyLawn extends ListenerPlugin with CommandsPlugin with Cubes {
 
-  val listener = OnPlayerMove((p, e) => cubes.foreach{ case (owner, lawn) =>
-    if (p != owner and lawn.contains(e.getTo) and ! lawn.contains(e.getFrom))
-      owner ! (p.name + " is on your lawn!")
-  })
+  def movingOntoLawn(e:PlayerMoveEvent, lawn: Cube) =
+    lawn.contains(e.getTo) && ! lawn.contains(e.getFrom)
+  
+  val listener = OnPlayerMove((p, e) =>
+    for ((owner, lawn) <- cubes; if (p != owner && movingOntoLawn(e, lawn)))
+      owner ! (s"${p.name} is on your lawn!")
+  )
 
-  val commands = List(pos1, pos2, Command(
-    name = "/GetOffMyLawn",
-    desc = "Kick everyone off your lawn, and strikes them with lightning",
-    body = noArgs(owner => run(owner)(lawn => lawn.players.filter(_ != owner).foreach { p =>
-      p.teleportTo(p.world.getHighestBlockAt(p.world(lawn.maxX + 5, 0, lawn.maxZ + 5)))
-      p.strike
-      p ! (s"'Get off my lawn!!!', said ${owner.name}.")
-    }))
-  ))
+  val commands = List(
+    CommonCommands.pos1,
+    CommonCommands.pos2,
+    Command(
+      name = "GetOffMyLawn",
+      desc = "Kick everyone off your lawn, and strikes them with lightning",
+      body = noArgs(owner => run(owner)(lawn =>
+        for(p <- lawn.players; if(p != owner)) {
+          p.teleportTo(p.world.getHighestBlockAt(p.world(lawn.maxX + 5, 0, lawn.maxZ + 5)))
+          p.strike
+          p ! (s"'Get off my lawn!!!', said ${owner.name}.")
+        }
+      ))
+    )
+  )
 }
