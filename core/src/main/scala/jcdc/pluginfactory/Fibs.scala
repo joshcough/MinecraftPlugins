acc2c1be186f0ecd13f42b86dcc1824c97257852
joshcough <joshcough@gmail.com>
2012-09-29 19:01:02 -0400

    working on a non-memoizing stream, but have a minor bug...branching for now

---
+++ core/src/main/scala/jcdc/pluginfactory/Fibs.scala
@@ -0,0 +1,12 @@
+package jcdc.pluginfactory
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: joshcough
+ * Date: 9/29/12
+ * Time: 5:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Fibs {
+
+}
