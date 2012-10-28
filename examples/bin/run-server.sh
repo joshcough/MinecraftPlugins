0d6843d17fde9818ca9a8fcf55c003b9e5f9415e
joshcough <joshcough@gmail.com>
2012-10-28 16:32:46 -0400

    fixed

--- examples/bin/run-server.sh
+++ examples/bin/run-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $BUKKIT
-java -Djava.ext.dirs=lib:/Library/Java/Home/lib/ext/ -Xmx1024M -Xms512M -jar craftbukkit-1.3.2-R1.0.jar nogui
+#java -Djava.ext.dirs=lib:/Library/Java/Home/lib/ext/ -Xmx1024M -Xms512M -jar bukkit.jar nogui
+java -Djava.ext.dirs=lib:/Library/Java/Home/lib/ext/ -Xmx512M -Xms256M -jar bukkit.jar nogui
