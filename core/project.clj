b484c8d374f27f6b349938bf9c8f3c8f95019c16
joshcough <joshcough@gmail.com>
2012-12-24 13:42:46 -0500

    minor cleanup

--- core/project.clj
+++ core/project.clj
@@ -1,8 +1,12 @@
 ; i use this to uberjar up jcdc-pluginfactory, which is then used as a
-; project dependency for all my other scala plugins, so that scala is on their classpath.
+; project dependency for all my other plugins,
+; so that scala, clojure and all the jcdc.pluginfactory classes are available (on the classpath)
+; for all my plugins.
 (defproject jcdc.pluginfactory "0.1.0"
-  :resource-paths ["target/scala-2.10/classes/"]   ; puts plugin.xml in the jar.
-  :target-path    "target/"                        ; put the jar file in the target dir.
+  ; puts plugin.xml, and all jcdc.pluginfactory classes in the jar.
+  :resource-paths ["target/scala-2.10/classes/"]
+  ; put the jar file in the target dir.
+  :target-path    "target/"
   :dependencies   [
     [org.clojure/clojure          "1.4.0"]
     [org.scala-lang/scala-library "2.10.0-RC5"]
