7d152cc79af36de736f80e09e98316aae5dd9f0b
joshcough <joshcough@gmail.com>
2012-12-28 21:52:52 -0500

    added release script

---
+++ core/bin/release.sh
@@ -0,0 +1,12 @@
+
+echo "building scala code"
+./sbt publish-local
+
+echo "creating release jar"
+lein uberjar
+
+echo "copying documentation to pages dir"
+rm -rf ../../pages/MinecraftPlugins/scaladoc/scala-plugin-api_2.10.0-RC5-0.2.1
+cp -r ./target/scala-2.10/api ../../pages/MinecraftPlugins/scaladoc/scala-plugin-api_2.10.0-RC5-0.2.1
+
+echo "don't forget to commit the docs, and upload the release"
