e60396a181dadc6fd47016d1607e2a8ea1de133d
joshcough <joshcough@gmail.com>
2013-01-03 22:08:48 -0500

    changed Cube to be polymorphic, and introduced MineCraftCube, which is a Cube[Block]

---
+++ core/src/main/scala/jcdc/pluginfactory/MineCraftCube.scala
@@ -0,0 +1,12 @@
+package jcdc.pluginfactory
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: joshcough
+ * Date: 1/3/13
+ * Time: 9:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class MineCraftCube {
+
+}
