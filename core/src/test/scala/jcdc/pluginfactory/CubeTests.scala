5c40cd52dabc302a162c931b586482785ec7994f
joshcough <joshcough@gmail.com>
2013-01-14 22:14:13 -0500

    finally using scalacheck generators for cubes, and added lots of nice commutativity tests, and other tests. also added one simple test for the cube applicative, but there needs to be more.

--- core/src/test/scala/jcdc/pluginfactory/CubeTests.scala
+++ core/src/test/scala/jcdc/pluginfactory/CubeTests.scala
@@ -1,11 +1,88 @@
 package jcdc.pluginfactory
 
-import org.scalacheck.Properties
+import org.scalacheck._
 import org.scalacheck.Prop._
+import Gen._
+import Arbitrary.arbitrary
 
-object CubeTests extends Properties("Cube Tests") with TestHelpers {
+/**
+ * Generate cubes, possibly up to the maximum sized cube.
+ */
+trait CubeGenerators {
+  type C = Cube[Coor]
+  implicit val genCoor: Gen[Coor] = for {
+    x <- arbitrary[Int]; y <- arbitrary[Int]; z <- arbitrary[Int]
+  } yield Coor(x, y, z)
+  implicit val genCube: Gen[C] = for { c1 <- genCoor; c2 <- genCoor } yield Cube(c1, c2)(identity)
+  implicit val cubes = Arbitrary(genCube)
+}
 
+/**
+ * Generate cubes with:
+ *   max x, y, and z no greater than 50
+ *   min x, y, and z no less than -50
+ */
+trait SmallCubeGenerators { self: Properties =>
+  type C = Cube[Coor]
+  val smallInteger = Gen.choose(-50,50)
+  val genSmallCube: Gen[C] = for {
+    x1 <- smallInteger; y1 <- smallInteger; z1 <- smallInteger
+    x2 <- smallInteger; y2 <- smallInteger; z2 <- smallInteger
+  } yield Cube(Coor(x1, y1, z1), Coor(x2, y2, z2))(identity)
+  implicit val smallCubes = Arbitrary(genSmallCube)
+}
+
+class CubeTestBase(name: String) extends Properties(name) with TestHelpers{
+  def toList(c: Cube[Coor]) = c.toStream.toList.map(_.xyz)
+  def run(c: Cube[Coor]) = c.toZippedStream.toList.map( t => (t._1.xyz, t._2.xyz) )
+}
+
+/**
+ * This object is for testing general properties on cubes,
+ * where those properties are simple (O(1), usually), and calculating them doesn't
+ * require traversing the whole cube.
+ */
+object CubeTestsAwesome extends CubeTestBase("Cube Tests Awesome") with CubeGenerators {
+  test("cubes size > 0")(forAll{ (c:C) => c.size > 0 })
+}
+
+/**
+ * This object is for testing operations on random cubes,
+ * where those operations must traverse the cube.
+ * That is why we use SmallCubeGenerators, so we don't generate ridiculously huge cubes
+ */
+object SmallCubeTestsAwesome extends CubeTestBase("Cube Tests Awesome") with SmallCubeGenerators {
+
+  test("paste then mirror y same as mirror y then paste")(forAll{ (c:C) =>
+    run(c.translateTo(Coor(0, 10, 0)).mirrorY) ?= run(c.mirrorY.translateTo(Coor(0, 10, 0)))
+  })
+
+  test("mirror x y z, same as mirror z y x")(forAll{ (c:C) =>
+    run(c.mirrorX.mirrorY.mirrorZ) ?= run(c.mirrorZ.mirrorY.mirrorX)
+  })
+
+  test("mirror x x, same as identity")(forAll{ (c:C) =>
+    run(c.mirrorX.mirrorX) ?= run(c)
+  })
+}
+
+
+/**
+ * Literal tests that don't use the generators.
+ */
+object LiteralCubeTests extends CubeTestBase("Cube Tests") {
+
+  val c0 = Cube.coors((0,0,0),(0,0,0))
   val c = Cube.coors((0,0,0),(10,10,10))
+
+  test("ap") {
+    Cube.ap(
+      Cube((0,0,0),(0,0,0))(c => (p:P) => p(c)),
+      Cube[P]((0,0,0),(0,0,0))(c => X)
+    )(Coor(0,0,0)) ?= 0
+  }
+
+  test("size 0"){ c0.size ?= 1 }
 
   test("simple")   { c             ?= Cube.coors((0,0,0),(10,10,10)) }
   test("shift up") { c.shiftUp (1) ?= Cube.coors((0,1,0),(10,11,10)) }
@@ -31,14 +108,14 @@ object CubeTests extends Properties("Cube Tests") with TestHelpers {
   }})
 }
 
-object CubeMirroringTests extends Properties("Cube Mirroring Tests") with TestHelpers {
+/**
+ * Literal tests on mirroring that don't use the generators.
+ */
+object LiteralCubeMirroringTests extends CubeTestBase("Cube Mirroring Tests") {
 
   val cx = Cube.coors((0,0,0),(3,0,0))
   val cy = Cube.coors((0,0,0),(0,3,0))
   val cz = Cube.coors((0,0,0),(0,0,3))
-
-  def toList(c: Cube[Coor]) = c.toStream.toList.map(_.xyz)
-  def run(c: Cube[Coor]) = c.toZippedStream.toList.map( t => (t._1.xyz, t._2.xyz) )
 
   test("normal x") { toList(cx)         ?= List((0,0,0),(1,0,0),(2,0,0),(3,0,0)) }
   test("normal y") { toList(cy)         ?= List((0,0,0),(0,1,0),(0,2,0),(0,3,0)) }
