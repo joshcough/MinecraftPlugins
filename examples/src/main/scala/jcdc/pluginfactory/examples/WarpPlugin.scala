603c4da991b58011b4419d94b52ce9fc2d857af6
joshcough <joshcough@gmail.com>
2013-02-02 19:00:40 -0500

    minor refactoring

--- examples/src/main/scala/jcdc/pluginfactory/examples/WarpPlugin.scala
+++ examples/src/main/scala/jcdc/pluginfactory/examples/WarpPlugin.scala
@@ -20,7 +20,7 @@ import org.squeryl.PrimitiveTypeMode._
 class WarpPlugin extends DBPluginWithCommands {
 
   lazy val db = new Schema {
-    val warps = table[Warp]
+    private val warps = table[Warp]
     // insert a warp
     def insert(w: Warp) = warps.insert(w)
     // find all the warps
@@ -30,36 +30,32 @@ class WarpPlugin extends DBPluginWithCommands {
     // find all the warps for the given player
     def warpsFor(p: Player): List[Warp] =
       from(warps)(w => where(w.player === p.name) select(w)).toList
-    // find a warp
-    def findWarp(p: Player, warpName: String): Option[Warp] =
-      from(warps)(w => where(w.player === p.name and w.name === warpName) select(w)).headOption
     // Tries to find a warp for the given player, and then runs a function on it
     // (such as warping to it, or deleting it)
     def withWarp(p: Player, warpName: String)(f: Warp => Unit) =
-      findWarp(p, warpName).fold(p ! s"No such warp $warpName")(f)
+      from(warps)(w => where(w.player === p.name and w.name === warpName) select(w)).
+        headOption.fold(p ! s"No such warp $warpName")(f)
   }
-
-  import db._
 
   def warpToken = anyStringAs("warp-name")
 
   val commands = List(
-    DBCommand("warps", "List all warps.")(p => warpsFor(p).foreach(w => p ! w.toString)),
+    DBCommand("warps", "List all warps.")(p => db.warpsFor(p).foreach(w => p ! w.toString)),
     DBCommand("warp",  "Warp to the given warp location.", warpToken){ case (p, wt) =>
-      withWarp(p, wt)(w => p teleport w.location(p.world))
+      db.withWarp(p, wt)(w => p teleport w.location(p.world))
     },
     DBCommand("set-warp", "Create a new warp location.", warpToken){ case (p, warpName) =>
-      insert(Warp(0, warpName, p.name, p.x, p.y, p.z))
+      db.insert(Warp(0, warpName, p.name, p.x, p.y, p.z))
       p ! s"created warp: $warpName"
     },
     DBCommand("delete-warp", "Delete a warp location.", warpToken){ case (p, wt) =>
-      withWarp(p, wt){w => db.delete(w); p ! s"deleted warp: ${w.name}" }
+      db.withWarp(p, wt){w => db.delete(w); p ! s"deleted warp: ${w.name}" }
     },
     DBCommand("delete-all", "Delete all your warps.")(p =>
-      warpsFor(p).foreach{ w => p ! s"deleting: $w"; db.delete(w); }
+      db.warpsFor(p).foreach{ w => p ! s"deleting: $w"; db.delete(w); }
     ),
     OpOnly(DBCommand("purge-warps-database", "Delete all warps in the database.")(p =>
-      allWarps.foreach { w => p ! s"deleting: $w"; db.delete(w) })
+      db.allWarps.foreach { w => p ! s"deleting: $w"; db.delete(w) })
     )
   )
 }
