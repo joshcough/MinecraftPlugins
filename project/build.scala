f7179c8646da51b6245c252cd50aa5e01780bfeb
Josh Cough <joshcough@gmail.com>
2013-07-29 17:47:52 -0400

    oops

--- project/build.scala
+++ project/build.scala
@@ -127,60 +127,60 @@ object build extends Build {
   )
 
   // ErmineCraft stuff below.
-//  val repl = InputKey[Unit]("repl", "Run the Ermine read-eval-print loop")
-//  val allUnmanagedResourceDirectories = SettingKey[Seq[File]]("all-unmanaged-resource-directories", "unmanaged-resource-directories, transitively.")
-//  /** Multiply a setting across Compile, Test, Runtime. */
-//  def compileTestRuntime[A](f: Configuration => Setting[A]): SettingsDefinition =
-//    seq(f(Compile), f(Test), f(Runtime))
-//  def fromGithub(githubUser: String, project: String, subProject: Option[String] = None, sha: Option[String] = None) = {
-//    // if a specific commit isnt supplied, just fetch the very latest commit.
-//    // 'sbt update' doesn't seem to get the latest even though this says that it should
-//    // http://stackoverflow.com/questions/8864317/how-do-i-refresh-updated-git-dependency-artifacts-in-sbt
-//    // so instead we have to go to github and get the latest version.
-//    val shaOrLatest = sha.getOrElse{
-//      val commitsUrl = "https://api.github.com/repos/"+githubUser+"/"+project+"/commits?sha=master"
-//      scala.io.Source.fromURL(commitsUrl).takeWhile(_ != ',').mkString.dropWhile(_!=':').drop(2).dropRight(1)
-//    }
-//    val projectUri = uri("https://github.com/"+githubUser+"/"+project+".git#" + shaOrLatest)
-//    subProject match {
-//      case None => RootProject(projectUri)
-//      case Some(sub) => ProjectRef(projectUri, sub)
-//    }
-//  }
-//
-//  lazy val ermineLegacy  = fromGithub("ermine-language", "ermine-legacy")
-//
-//  lazy val ermine = {
-//    val ermineFileSettings = Defaults.defaultSettings ++ Seq[SettingsDefinition](
-//      compileTestRuntime(sc => classpathConfiguration in sc := sc)
-//     ,mainClass in (Compile, run) := Some("com.clarifi.reporting.ermine.session.Console")
-//     ,compileTestRuntime(sco => allUnmanagedResourceDirectories in sco <<=
-//        (Defaults.inDependencies(unmanagedResourceDirectories in sco, _ => Seq.empty)
-//         (_.reverse.flatten)))
-//      // Usually, resources end up in the classpath by virtue of `compile'
-//      // copying them into target/scala-*/classes, and from there into jar.  But
-//      // we want in development p(1) I can edit an Ermine module in src
-//      // resources, hit reload, and it's seen, and (2) I can edit CSS/JS, reload
-//      // the HTML, and it's seen.  So we (harmlessly) patch the src resources
-//      // dirs in *before* the classes dirs, so they will win in the classloader
-//      // lookup.
-//     ,compileTestRuntime(sco =>
-//        fullClasspath in sco <<= (allUnmanagedResourceDirectories in sco,
-//                                  fullClasspath in sco) map {
-//          (urd, fc) => Attributed.blankSeq(urd) ++ fc
-//      })
-//    ) flatMap (_.settings)
-//    Project(
-//      id = "ermine-plugins",
-//      base = file("ermine"),
-//      settings = standardSettings ++ Seq[Sett](
-//        name := "Ermine Plugin API",
-//        //libraryDependencies ++= Seq("com.clarifi" %% "ermine-legacy" % "0.1"),
-//        fullRunInputTask(repl, Compile, "com.clarifi.reporting.ermine.session.Console")
-//      ) ++ ermineFileSettings,
-//      dependencies = Seq(core)
-//    )
-//  }.dependsOn(ermineLegacy)
+  val repl = InputKey[Unit]("repl", "Run the Ermine read-eval-print loop")
+  val allUnmanagedResourceDirectories = SettingKey[Seq[File]]("all-unmanaged-resource-directories", "unmanaged-resource-directories, transitively.")
+  /** Multiply a setting across Compile, Test, Runtime. */
+  def compileTestRuntime[A](f: Configuration => Setting[A]): SettingsDefinition =
+    seq(f(Compile), f(Test), f(Runtime))
+  def fromGithub(githubUser: String, project: String, subProject: Option[String] = None, sha: Option[String] = None) = {
+    // if a specific commit isnt supplied, just fetch the very latest commit.
+    // 'sbt update' doesn't seem to get the latest even though this says that it should
+    // http://stackoverflow.com/questions/8864317/how-do-i-refresh-updated-git-dependency-artifacts-in-sbt
+    // so instead we have to go to github and get the latest version.
+    val shaOrLatest = sha.getOrElse{
+      val commitsUrl = "https://api.github.com/repos/"+githubUser+"/"+project+"/commits?sha=master"
+      scala.io.Source.fromURL(commitsUrl).takeWhile(_ != ',').mkString.dropWhile(_!=':').drop(2).dropRight(1)
+    }
+    val projectUri = uri("https://github.com/"+githubUser+"/"+project+".git#" + shaOrLatest)
+    subProject match {
+      case None => RootProject(projectUri)
+      case Some(sub) => ProjectRef(projectUri, sub)
+    }
+  }
+
+  lazy val ermineLegacy  = fromGithub("ermine-language", "ermine-legacy")
+
+  lazy val ermine = {
+    val ermineFileSettings = Defaults.defaultSettings ++ Seq[SettingsDefinition](
+      compileTestRuntime(sc => classpathConfiguration in sc := sc)
+     ,mainClass in (Compile, run) := Some("com.clarifi.reporting.ermine.session.Console")
+     ,compileTestRuntime(sco => allUnmanagedResourceDirectories in sco <<=
+        (Defaults.inDependencies(unmanagedResourceDirectories in sco, _ => Seq.empty)
+         (_.reverse.flatten)))
+      // Usually, resources end up in the classpath by virtue of `compile'
+      // copying them into target/scala-*/classes, and from there into jar.  But
+      // we want in development p(1) I can edit an Ermine module in src
+      // resources, hit reload, and it's seen, and (2) I can edit CSS/JS, reload
+      // the HTML, and it's seen.  So we (harmlessly) patch the src resources
+      // dirs in *before* the classes dirs, so they will win in the classloader
+      // lookup.
+     ,compileTestRuntime(sco =>
+        fullClasspath in sco <<= (allUnmanagedResourceDirectories in sco,
+                                  fullClasspath in sco) map {
+          (urd, fc) => Attributed.blankSeq(urd) ++ fc
+      })
+    ) flatMap (_.settings)
+    Project(
+      id = "ermine-plugins",
+      base = file("ermine"),
+      settings = standardSettings ++ Seq[Sett](
+        name := "Ermine Plugin API",
+        //libraryDependencies ++= Seq("com.clarifi" %% "ermine-legacy" % "0.1"),
+        fullRunInputTask(repl, Compile, "com.clarifi.reporting.ermine.session.Console")
+      ) ++ ermineFileSettings,
+      dependencies = Seq(core)
+    )
+  }.dependsOn(ermineLegacy)
 }
 
 // some crap left over from old build.sbt files.
