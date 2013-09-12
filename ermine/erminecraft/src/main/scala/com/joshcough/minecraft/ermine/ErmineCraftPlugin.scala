1cd21e745a40b556a62da38d817b4004e3bda554
joshcough <joshcough@gmail.com>
2013-09-12 03:26:35 -0400

    added yml generation function to ErmineCraftPlugin

--- ermine/erminecraft/src/main/scala/com/joshcough/minecraft/ermine/ErmineCraftPlugin.scala
+++ ermine/erminecraft/src/main/scala/com/joshcough/minecraft/ermine/ErmineCraftPlugin.scala
@@ -10,9 +10,14 @@ import com.clarifi.reporting.ermine.session.FilesystemReportsCache
 class ErmineCraftPlugin extends ScalaPlugin {
 
   lazy val moduleName = this.name
-  lazy val ermineModule = runErmine(moduleName, "plugin")
+  lazy val ermineModule = runErmine(moduleName, "plugin")()
   lazy val listeners: List[Listener] =
-    runErmine("Minecraft.Minecraft", "listeners", ermineModule).extract[List[Listener]]
+    runErmine("Minecraft.Minecraft", "listeners")(ermineModule).extract[List[Listener]]
+
+  override def yml(author: String, version: String): String =
+    runErmine("Minecraft.Minecraft", "yml")(
+      ermineModule, Prim(this.name), Prim(this.getClass.getName), Prim(author), Prim(version)
+    ).extract[String]
 
   override def onEnable{ super.onEnable(); cache; listeners.foreach(registerListener) }
 
@@ -20,24 +25,16 @@ class ErmineCraftPlugin extends ScalaPlugin {
                          commandName: String, args: Array[String]) = {
     println(s"$name handling $commandName [${args.mkString(",")}]")
     //onCommand : ErminePlugin -> Player -> BukkitCommand -> String -> List String -> IO ()
-    runIO(runErmine(
-      "Minecraft.Minecraft",
-      "onCommand",
-      ermineModule,
-      Prim(sender.asInstanceOf[Player]),
-      Prim(cmd),
-      Prim(commandName),
-      Prim(args.toList)
+    runIO(runErmine("Minecraft.Minecraft", "onCommand")(
+      ermineModule, Prim(sender.asInstanceOf[Player]), Prim(cmd), Prim(commandName), Prim(args.toList)
     ))
     true
   }
 
-  override def dependencies = List("ErmineLibPlugin")
-
   // code for running ermine
   val ermineDir = "/Users/joshcough/work/MinecraftPlugins/ermine/src/main/resources/modules"
   lazy val cache = new FilesystemReportsCache(List(ermineDir))
-  def runErmine(module: String, function: String, args: Runtime*): Runtime =
+  def runErmine(module: String, function: String)(args: Runtime*): Runtime =
     cache.getReport(module, function).toEither match {
       case Right(r) => r(args:_*)
       case Left(e)  => throw new RuntimeException(s"Couldn't find $moduleName.plugin", e)
