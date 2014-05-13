ca049699a08bd2a99e81086c8afa5eb0f59ce7ff
joshcough <joshcough@gmail.com>
2014-05-12 22:41:42 -0400

    upgraded sbt-assembly, fixed up intellij file

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,6 +4,6 @@ addSbtPlugin("com.joshcough" % "scala-minecraft-yml-gen" % "0.3.1")
 
 addSbtPlugin("me.lessis" % "bintray-sbt" % "0.1.0")
 
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.2")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.11.2")
 
 seq(bintrayResolverSettings:_*)
--- scala/examples/MultiPlayerCommands/MultiPlayerCommands.scala
+++ scala/examples/MultiPlayerCommands/MultiPlayerCommands.scala
@@ -5,6 +5,7 @@ import org.bukkit.Material._
 import org.bukkit.entity.EntityType._
 import scala.collection.JavaConversions._
 import com.joshcough.minecraft.CommandsPlugin
+import org.bukkit.Material
 
 /**
  * Classic MultiPlayerCommands plugin, done in Scala.
@@ -48,12 +49,16 @@ class MultiPlayerCommands extends CommandsPlugin {
       p.blocksAround.foreach(_ changeTo m)
     },
 
-    Command("safe",     "Put yourself in a box made of bedrock.")(
+    Command("safe",      "Put yourself in a box made of bedrock.")(
       _.blocksAround.foreach(_ changeTo BEDROCK)
     ),
 
+    OpOnly(P2P("punish", "Puts someone in a box made of bedrock.")((you, them) =>
+      you.doTo(them, them.blocksAround.foreach(_ changeTo BEDROCK), "punished")
+    )),
+
     Command("drill",    "Drill down to bedrock immediately.")(p =>
-      for (b <- p.blockOn.blocksBelow.takeWhile(_ isNot BEDROCK); if (b isNot AIR)) {
+      for (b <- p.blockOn.blocksBelow.takeWhile(_ isNot BEDROCK); if b isNot AIR) {
         b.erase
         if (b.blockBelow is BEDROCK) b nthBlockAbove 2 changeTo STATIONARY_WATER
       }
@@ -77,6 +82,46 @@ class MultiPlayerCommands extends CommandsPlugin {
       you.doTo(them, them setFoodLevel 0, "fed"))
     ),
 
-    OpOnly(P2P("shock",    "Shock a player.")((you, them) => you.doTo(them, them.shock, "shocked")))
+    OpOnly(P2P("shock",    "Shock a player.")((you, them) => you.doTo(them, them.shock, "shocked"))),
+
+    OpOnly(Command("lamp", "Spawns a lamp wherever you are looking."){ p =>
+      val b = p.getTargetBlock(null, 1000)
+      if((b is Material.AIR) || math.abs(p.x - b.x) > 50 || math.abs(p.z - b.z) > 50)
+        p ! s"That's more than 50 blocks away. (x: ${math.abs(p.x - b.x)}) (z: ${math.abs(p.z - b.z)})"
+      else {
+        b.blockBelow.changeTo(Material.REDSTONE_TORCH_ON)
+        b.changeTo(Material.REDSTONE_LAMP_ON)
+      }
+    }),
+
+    Command("jump", "Jump really high.", (int ~ int ~ int) or eof){
+      case (p, Left(x ~ y ~ z)) => p.setVelocity(new org.bukkit.util.Vector(x, y, z))
+      case (p, Right(_))        => p.setVelocity(new org.bukkit.util.Vector(0, 10, 0))
+    }
   )
 }
+
+/**
+
+p = player
+t = target
+
+looking down x axis:
+  p.x = 0, t.x = 10, p.z = 0, t.z = 0
+  put all the weight in x: Vector(x=10, y=10, z=0)
+
+looking down z axis:
+  p.z = 0, t.z = 10, p.x = 0, t.x = 0
+  put all the weight in z: Vector(x=0, y=10, z=10)
+
+looking in between
+  p.x = 0, p.z = 10, t.x = 5, t.z = 5
+  put equal weight in both: Vector(x=10, y=10, z=10)
+
+looking more towards the x axis:
+  p.x = 0, p.z = 10, t.x = 7.5, t.z = 2.5
+  hmm...use those weights?:  Vector(x=7.5, y=10, z=2.5)
+
+similar with z axis.
+
+*/
