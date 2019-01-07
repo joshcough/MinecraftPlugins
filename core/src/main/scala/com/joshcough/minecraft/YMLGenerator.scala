6074c6869f5ba790cf48f11af1fed6a83418ef3e
Josh Cough <joshcough@gmail.com>
2019-01-06 23:11:52 -0500

    made YMLGenerator main function nice, using argsParser

--- core/src/main/scala/com/joshcough/minecraft/YMLGenerator.scala
+++ core/src/main/scala/com/joshcough/minecraft/YMLGenerator.scala
@@ -16,24 +16,29 @@ object YMLGenerator {
     anyString.named("author")    ~
     anyString.named("version")   ~
     anyString.named("outputDir") ~
+    anyString.named("configClassName") ~
     eof
 
-  //  def create[T](className: String) = Class.forName(className).newInstance.asInstanceOf[T]
-  //  def generateYml(className: String, author: String, version: String, outputDir: String) =
-  //    create[ScalaPlugin](className).writeYML(author, version, outputDir)
-  //
-  //  def main(args: Array[String]): Unit = {
-  //    argsParser(args.toList).extract {
-  //      case className ~ author ~ version ~ outputDir ~ _ => generateYml(className, author, version, outputDir)
-  //    }
-  //  }
+  def main(args: Array[String]): Unit = argsParser(args.toList).extract {
+    case pluginName ~ author ~ version ~ outputDir ~ configClassName ~ _ =>
+      writeYMLFromConfig(pluginName, author, version, outputDir, configClassName)
+  }
 
-  def writeYML( pluginName: String,
-                pluginClassName: String,
-                author: String,
-                version: String,
-                outputDir: String = ".",
-                commands: List[CommandsPlugin.Command]): Unit = {
+  def writeYMLFromConfig(pluginName: String,
+                         author: String,
+                         version: String,
+                         outputDir: String = ".",
+                         configClassName: String): Unit = {
+    val config = Class.forName(configClassName).newInstance().asInstanceOf[PluginConfig[_]]
+    writeYML(pluginName, config.pluginClass.getName, author, version, outputDir, config.commands)
+  }
+
+  def writeYML(pluginName: String,
+               pluginClassName: String,
+               author: String,
+               version: String,
+               outputDir: String = ".",
+               commands: List[CommandsPlugin.Command]): Unit = {
     val resources = new java.io.File(outputDir)
     resources.mkdirs
     println(resources.getAbsolutePath)
@@ -43,35 +48,15 @@ object YMLGenerator {
       f.close()
     }
     val yml = CommandsPlugin.fullPluginYml(
-        pluginName,
-        pluginClassName,
-        author,
-        version,
-        Nil, List("ScalaLibPlugin", "ScalaPluginAPI"), Nil,
-        commands)
+      pluginName,
+      pluginClassName,
+      author,
+      version,
+      Nil,
+      // TODO: these should not be hardcoded...should come from the config
+      List("ScalaLibPlugin", "ScalaPluginAPI"), Nil,
+      commands)
     println(yml)
     write(yml, "plugin.yml")
-  }
-
-  def writeYML(args: Array[String],
-               pluginClassName: String,
-               commands: List[CommandsPlugin.Command]): Unit = args match {
-    case Array(pluginName, author, version, outputDir) =>
-      writeYML(pluginName, pluginClassName, author, version, outputDir, commands)
-  }
-
-  def writeYMLFromConfig(pluginName: String,
-                         author: String,
-                         version: String,
-                         outputDir: String = ".",
-                         configClassName: String): Unit = {
-    println("configClassName: " + configClassName)
-    val config = Class.forName(configClassName).newInstance().asInstanceOf[PluginConfig[_]]
-    writeYML(pluginName, config.pluginClass.getName, author, version, outputDir, config.commands)
-  }
-
-  def main(args: Array[String]): Unit = args match {
-    case Array(pluginName, author, version, outputDir, configClassName) =>
-      writeYMLFromConfig(pluginName, author, version, outputDir, configClassName)
   }
 }
